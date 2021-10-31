e423d928f17c5d3c6eeb0a097348cc0e78683811
Lucas <lcmoraes94@hotmail.com>
2021-10-31 17:42:48 -0300

    ajuste urls de chamada e imagem

--- home/home.js
+++ home/home.js
@@ -2,9 +2,9 @@ const idce = document.querySelector('#btn-idce');
 const mpacs = document.querySelector('#btn-mpacs');
 
 mpacs.addEventListener('click', () => {
-    window.location = "/geradorpacs/index.html";
+    window.location = "https://lucomoraes.github.io/GeradorQuery/geradorpacs/index.html";
 });
 
 idce.addEventListener('click', () => {
-    window.location = "/geradoridce/index.html";
+    window.location = "https://lucomoraes.github.io/GeradorQuery/geradoridce/index.html";
 });
